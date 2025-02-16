30ccfbe4f37c1cd7f659ff64a7f362ff46fc5f75
sourav6251 <dassourav3738+github@gmail.com>
2025-02-16 11:59:50 +0530

    fix

--- Form/index.js
+++ Form/index.js
@@ -17,7 +17,6 @@ function submits() {
   const formOutput = document.getElementsByClassName("output_container")[0];
   formOutput.style.display = "flex";
   const outputHTML = `
-       <!--  <div class="output-data"> -->
             <div class="output-data-content">
                 <h2>User Details</h2>
                 <p><strong>Full Name:</strong> ${fullName}</p>
@@ -28,27 +27,22 @@ function submits() {
                 <p><strong>Phone:</strong> ${phone}</p>
             </div>
             <button class="delete-button" onclick="deleteSubmission(this)">  <img src="./image/dustbinm.svg" alt="delete"></button>
-        <!--// </div>-->
     `;
 
   // Create a new div element
   const newSubmission = document.createElement("div");
-//   newSubmission.classList.add("submission-class");
   newSubmission.classList.add("output-data");
   newSubmission.innerHTML = outputHTML;
 
-  // Prepend new submission to the top of output container
   formOutput.insertBefore(newSubmission, formOutput.firstChild);
 }
 
 function deleteSubmission(button) {
-    // Get the parent output-data div and remove it
     const outputData = button.closest('.output-data');
     if (outputData) {
         outputData.remove();
     }
     
-    // Hide the output container if no more submissions exist
     const formOutput = document.getElementsByClassName("output_container")[0];
     if (!formOutput.hasChildNodes()) {
         console.log("No submissions left.");
